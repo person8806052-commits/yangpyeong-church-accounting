4cf871599a0ed4c215bb5cd8d65e667fb1e85e0e
person8806052-commits <person8806052@gmail.com>
2026-09-15 19:00:56 +0900

    Delete scripts directory

--- scripts/setup-native.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-npm install
-if [ ! -d android ]; then npx cap add android; fi
-if [ "$(uname -s)" = "Darwin" ] && [ ! -d ios ]; then npx cap add ios; fi
-npx cap sync
-printf '\nNative platforms are ready.\n'
